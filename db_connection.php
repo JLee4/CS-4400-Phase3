935b1af0828067eeb72d4b1a6216f4d1f905604c
Johnny <leejohnny47@gmail.com>
2017-12-02 11:59:25 -0500

    css doesn't look like penis now

--- db_connection.php
+++ db_connection.php
@@ -6,33 +6,5 @@ $host = 'academic-mysql.cc.gatech.edu';
 $database = 'cs4400_Group_81';
 
 $connection = mysqli_connect($host, $username, $password, $database) or die("Cannot connect to DB");
-$result = $connection->query("SELECT * FROM User");
-echo "<table>";
-while ($row = $result->fetch_assoc()) {
-    echo "<tr><td>".$row['Username']."</td><td>".$row['Password']."</td></tr>";
-}
-echo "</table>";
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login Page</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
-<body>
-<div id="window">
-    <form name="form" method="POST">
-        <p>
-            <label>Username:</label>
-            <input type="text" id="user" name="user">
-        </p>
-        <p>
-            <label>Password:</label>
-            <input type="password" id="pass" name="pass">
-        </p>
-
-</div>
-</body>
-</html>
