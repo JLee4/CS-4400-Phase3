e68e123971c6b11dde5b37f00cce915029e1515f
pkedarisetty3 <pranit.kedarisetty@gmail.com>
2017-12-03 11:23:25 -0500

    Hi

---
+++ passenger_flow_report.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pranit
+ * Date: 12/2/2017
+ * Time: 7:37 PM
+ */
+
+
+?>
---
+++ passenger_flow_report_form.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Passenger Flow Report</title>
+
+    <style>
+        table {
+            border-spacing: 0;
+            width: 50%;
+            border: 1px solid #ddd;
+        }
+
+        th {
+            cursor: pointer;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 16px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2
+        }
+    </style>
+</head>
+
+<body>
+<p><strong>Passenger Flow Report</strong></p>
+
+<table id="myTable">
+    <tr>
+        <!--When a header is clicked, run the sortTable function, with a parameter, 0 for sorting by names, 1 for sorting by country:-->
+        <th onclick="sortTable(0)">Station Name</th>
+        <th onclick="sortTable(1)"># Passengers In</th>
+        <th onclick="sortTable(2)"># Passengers Out</th>
+        <th onclick="sortTable(3)">Flow</th>
+        <th onclick="sortTable(4)">Revenue</th>
+    </tr>
+    <tr>
+        <td>Asshole</td>
+        <td>N1</td>
+        <td>2.00</td>
+        <td>Open</td>
+        <td>Train</td>
+    </tr>
+    <tr>
+        <td>Bitch</td>
+        <td>N2</td>
+        <td>2.50</td>
+        <td>Closed</td>
+        <td>Train</td>
+    </tr>
+    <tr>
+        <td>Cunt</td>
+        <td>N3</td>
+        <td>2.50</td>
+        <td>Open</td>
+        <td>Bus</td>
+    </tr>
+    <tr>
+        <td>Dick</td>
+        <td>N4</td>
+        <td>1.50</td>
+        <td>Closed</td>
+        <td>Bus</td>
+    </tr>
+    <tr>
+        <td>Eggplant</td>
+        <td>N5</td>
+        <td>5.00</td>
+        <td>Open</td>
+        <td>Train</td>
+    </tr>
+</table>
+
+<button type="button" onclick="alert('Update')">Update</button>
+<button type="button" onclick="alert('Reset')">Reset</button>
+
+</body>
+</html>
