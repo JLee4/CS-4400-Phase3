65cd11fd04a61b531430432c3331d697ad170eb9
Johnny <leejohnny47@gmail.com>
2017-12-03 14:11:20 -0500

    passenger menu coded

--- passenger_menu.php
+++ passenger_menu.php
@@ -18,6 +18,24 @@ include 'db_connection.php';
         };
         xmlhttp.send();
     }
+    function startTrip() {
+        if (document.getElementById("card_num").selectedIndex === 0) {
+            alert("A Breezecard must be selected");
+            return;
+        }
+        document.getElementById("start_trip_btn").disabled = true;
+        document.getElementById("end_trip_btn").disabled = false;
+        xmlhttp = new XMLHttpRequest();
+        var card_number = document.getElementById("card_num").options[document.getElementById("card_num").selectedIndex].text;
+        var start_trip = document.getElementById("start_trip").options[document.getElementById("start_trip").selectedIndex].text;
+        var end_trip = document.getElementById("end_trip").options[document.getElementById("end_trip").selectedIndex].text;
+        xmlhttp.open("GET", "balance_query.php?card="+ card_number + "&start=" +start_trip + "&end=" + end_trip,false);
+        xmlhttp.send();
+    }
+    function endTrip() {
+        document.getElementById("start_trip_btn").disabled = false;
+        document.getElementById("end_trip_btn").disabled = true;
+    }
 </script>
 <body>
 <div id="window">
@@ -35,6 +53,7 @@ include 'db_connection.php';
                 }
                 ?>
             </select>
+            <input type="button" id="manage_cards_btn"  value="Manage Cards" onclick="location.href='trip_history.php'">
         </p>
         <p>
             <label>Balance</label>
@@ -43,7 +62,7 @@ include 'db_connection.php';
         </p>
         <p>
             <label>Start At</label>
-            <select>
+            <select id="start_trip">
                 <?php
                 $query = "SELECT Name, EnterFare FROM Station";
                 $sql = mysqli_query($connection, $query);
@@ -51,10 +70,12 @@ include 'db_connection.php';
                     echo "<option value=\"owner1\">" . $row['Name'] . "</option>";
                 }
                 ?>
-            </select>        </p>
+            </select>
+            <input type="button" id="start_trip_btn"  value="Start Trip" onclick="startTrip()">
+        </p>
         <p>
             <label>End At</label>
-            <select>
+            <select id="end_trip">
                 <?php
                 $query = "SELECT Name, EnterFare FROM Station";
                 $sql = mysqli_query($connection, $query);
@@ -62,7 +83,9 @@ include 'db_connection.php';
                     echo "<option value=\"owner1\">" . $row['Name'] . "</option>";
                 }
                 ?>
-            </select>        </p>
+            </select>
+            <input type="button" id="end_trip_btn"  value="End Trip" onclick="endTrip()" disabled="true">
+        </p>
         <p>
             <input type="button" id="trip_history_btn"  value="View Trip History" onclick="location.href='trip_history.php'">
             <input type="button" id="logout_btn" value="Log Out" onclick="location.href='login_form.php'">
---
+++ trip_progress.php
@@ -0,0 +1,22 @@
+<?php
+include 'db_connection.php';
+$card = $_GET['card'];
+$start = $_GET['start'];
+$end_r = $_GET['end'];
+
+$value_result = mysqli_query($connection, "SELECT Value FROM Breezecard WHERE BreezecardNum = '$card'");
+$fare_result = mysqli_query($connection, "SELECT EnterFare FROM Station WHERE Name = '$start'");
+$value = $value_result->fetch_row()[0] - $fare_result->fetch_row()[0];
+$start_result = mysqli_query($connection, "SELECT StopID FROM Station WHERE Name = '$start'");
+$end_result = mysqli_query($connection, "SELECT StopID FROM Station WHERE Name = '$end'");
+$breezecard_value = mysqli_query($connection, "UPDATE Breezecard SET Value = '$value'") or die("DB error");
+
+$fare = $fare_result->fetch_row()[0];
+$time = date("Y-m-d H:i:s");
+$start = $start_result->fetch_row()[0];
+$end_r = $end_result->fetch_row()[0];
+
+$trip_result = mysqli_query($connection, "INSERT INTO Trip (Tripfare, StartTime, BreezecardNum, StartsAt, EndsAt)"." 
+  VALUES ('$fare', '$time', '$card', '$start', '$end_r')") or die("DB error");
+exit();
+?>
