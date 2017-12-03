37187e482b0ad8da000868dcdc9d131465baad4f
pkedarisetty3 <34079424+pkedarisetty3@users.noreply.github.com>
2017-12-03 16:28:44 -0500

    View Station

---
+++ view_station_form.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Station Detail - North Springs</title>
+
+    <style>
+        table {
+            border-spacing: 0;
+            width: 50%;
+            border: 1px solid #ddd;
+        }
+
+        th {
+            cursor: pointer;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 16px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2
+        }
+    </style>
+</head>
+
+<body>
+
+
+<p><strong>North Springs</strong></p>
+
+<p>
+    <label>Fare</label>
+    <input type="password" id="password" name="password">
+    <button type="button" onclick="alert('Update')">Update Fare</button>
+
+</p>
+<p>
+    <label>Nearest Intersection</label>
+</p>
+<p>
+    <input type="checkbox">Open Station<br>
+</p>
+
+<p>
+    <label> When checked, passengers can enter at this station.</label>
+</p>
+
+</body>
+</html>
